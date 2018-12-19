249472af77ca2ad119183176a23d868d555181bd
Adam Niedzwiecki <adamn8142@gmail.com>
2018-12-19 16:30:01 -0700

    Add park pop-up details

--- src/Park.js
+++ src/Park.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
 class Park extends Component {
- constructor() {
-   super();
+ constructor(props) {
+   super(props);
    this.state = {
 
    };
  }
+  showFullCard=() =>{
 
+  }
 
   render() {
+    let imagePath = `./${this.props.selectedPark.image}`;
     return (
-      <div>H3llo</div>
+      <div> 
+        <img></img>
+        <h1>{this.props.selectedPark.parkName} National Park</h1>
+        <img src={imagePath} />
+        {/* {imageSource} */}
+        <h3>{this.props.selectedPark.description.substring(0, 80)}...</h3>
+        <button onClick={this.showFullCard}>View More</button>
+        <img></img>
+        <img></img>
+      </div>
     )
   }
 }
