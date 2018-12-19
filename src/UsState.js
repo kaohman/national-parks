8f6013473d9596e521a8e8be6ce1aca9a094ff5b
Karin Ohman <kaohma@gmail.com>
2018-12-19 14:55:32 -0700

    Add spans to state info

--- src/UsState.js
+++ src/UsState.js
@@ -8,11 +8,11 @@ class UsState extends Component {
     return (
       <div>
         <h1>{this.props.name}</h1>
-        <h3>Capital: {this.props.selectedState.capitalCity}</h3>
-        <h3>Population: {this.props.selectedState.Population}</h3>
-        <h3>Number of Parks in State: {this.props.selectedState.nationalParks.length}</h3>
-        <h3>Date Established: {this.props.selectedState.dateEstablished}</h3>
-        <h3>State Flower: {this.props.selectedState.stateFlower}</h3>
+        <h3>Capital: <span>{this.props.selectedState.capitalCity}</span></h3>
+        <h3>Population: <span>{this.props.selectedState.Population}</span></h3>
+        <h3>Date Established: <span>{this.props.selectedState.dateEstablished}</span></h3>
+        <h3>Number of Parks in State: <span>{this.props.selectedState.nationalParks.length}</span></h3>
+        <h3>State Flower: <span>{this.props.selectedState.stateFlower}</span></h3>
       </div>
     )
   }
