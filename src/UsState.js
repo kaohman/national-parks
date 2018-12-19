e3ee9e99997d5f3e0961c72ce77111f2a81b0b97
Karin Ohman <kaohma@gmail.com>
2018-12-19 14:52:14 -0700

    Add additional details to state info

--- src/UsState.js
+++ src/UsState.js
@@ -8,9 +8,11 @@ class UsState extends Component {
     return (
       <div>
         <h1>{this.props.name}</h1>
-        <h3>{this.props.selectedState.stateFlower}</h3>
-        <h3></h3>
-        <h3></h3>
+        <h3>Capital: {this.props.selectedState.capitalCity}</h3>
+        <h3>Population: {this.props.selectedState.Population}</h3>
+        <h3>Number of Parks in State: {this.props.selectedState.nationalParks.length}</h3>
+        <h3>Date Established: {this.props.selectedState.dateEstablished}</h3>
+        <h3>State Flower: {this.props.selectedState.stateFlower}</h3>
       </div>
     )
   }
